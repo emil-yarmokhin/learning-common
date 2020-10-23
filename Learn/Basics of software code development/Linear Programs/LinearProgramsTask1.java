6de800d5617fa36dfdae251cc0dbb687cfdae290
emil-yarmokhin <emil.yarmokhin@gmail.com>
2020-10-23 14:49:54 +0300

    Удалил ненужное

--- Learn/Basics of software code development/Linear Programs/LinearProgramsTask1.java
+++
@@ -1,9 +0,0 @@
-public class LinearProgramsTask1 {
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 3;
-        int c = 5;
-        int z = ((a - 3) * b / 2) + c;
-        System.out.println(z);
-    }
-}
