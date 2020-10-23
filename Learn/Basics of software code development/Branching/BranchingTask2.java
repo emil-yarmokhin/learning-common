17c6f30829249bec06744a0d9a60d9961940a9b6
emil-yarmokhin <72553224+emil-yarmokhin@users.noreply.github.com>
2020-10-23 14:36:22 +0300

    Pull Request Branching

--- Learn/Basics of software code development/Branching/BranchingTask2.java
+++ Learn/Basics of software code development/Branching/BranchingTask2.java
@@ -1,13 +1,29 @@
+package Branching;
+
 public class BranchingTask2 {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
         int c = 3;
         int d = 4;
-        if (Math.min (a, b) > Math.min(c,d)) {
-            System.out.println(Math.min(a, b));
+        int min1;
+        int min2;
+        int maxmin1min2;
+        if (a < b) {
+            min1 = a;
         } else {
-            System.out.println(Math.min(c, d));
+            min1 = b;
         }
+        if (c < d) {
+            min2 = c;
+        } else {
+            min2 = d;
+        }
+        if (min1 > min2) {
+            maxmin1min2 = min1;
+        } else {
+            maxmin1min2 = min2;
+        }
+        System.out.println(maxmin1min2);
     }
 }
