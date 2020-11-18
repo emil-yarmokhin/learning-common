be61c6fa551655e97fdb0a168aa5736676ae492e
emil-yarmokhin <emil.yarmokhin@gmail.com>
2020-11-18 09:50:32 +0300

    Выполненный блок по одномерным массивам.

--- Learn/Learning EPAM/src/Section2Algorithmization/OdArrays/OdArraysTask9.java
+++ Learn/Learning EPAM/src/Section2Algorithmization/OdArrays/OdArraysTask9.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 // Если таких чисел несколько, то определить наименьшее из них.
 public class OdArraysTask9 {
     public static void main(String[] args) {
-        int[] a = {0,2,2,2,3,3,3,0,0,1,6};
+        int[] a = {0,2,2,3,3,1,1,1,1,6,0,0};
         int[] b = new int[a.length];
         int[] c = new int[a.length];
         int count = 1;
@@ -17,18 +17,20 @@ public class OdArraysTask9 {
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] == a[j]) {
+                    c[i] = 1;
                     b[i] = a[i];
-                    b[j] = a[j];
                     count++;
                     c[i] = count;
+                    maxTimes = c[i];
                 }
             }
             count = 1;
         }
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(c));
         //находим наиболее часто встречающееся число с наименьшим значением
         for (int i = 0; i < c.length; i++) {
             if (i == 0) {
-                maxTimes = c[i];
                 minElemMaxTimes = b[i];
             } else if (c[i] > maxTimes) {
                 maxTimes = c[i];
